f7b1b035957560920678c5e93a5d0567dad832da
adadidii <adinugroho97@gmail.com>
2023-11-27 16:57:39 +0700

    Develop the layouts of TaskList and TaskProgress pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import SideMenu from './components/SideMenu'
 import { RecoilRoot } from 'recoil'
 import TaskSummary from './features/tasks/components/TaskSummary'
+import TaskList from './features/tasks/components/TaskList/TaskList'
+import TaskProgress from './features/tasks/components/TaskProgress/TaskProgress'
 
 const router = createBrowserRouter([
   {
@@ -19,7 +21,7 @@ const router = createBrowserRouter([
     element: (
       <div style={{ display: 'flex' }}>
         <SideMenu /> {/* Ditambahkan */}
-        <h1>Task List</h1>
+        <TaskList />
       </div>
     ),
   },
@@ -28,7 +30,7 @@ const router = createBrowserRouter([
     element: (
       <div style={{ display: 'flex' }}>
         <SideMenu /> {/* Ditambahkan */}
-        <h1>Task Progress</h1>
+        <TaskProgress />
       </div>
     ),
   },
